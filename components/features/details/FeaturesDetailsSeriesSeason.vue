669da9b83cbf42f0a6aaeaa235bc21a587479877
mmreza <mmrezadev@gmail.com>
2025-07-19 20:58:29 +0330

    create :: create component for season details

---
+++ components/features/details/FeaturesDetailsSeriesSeason.vue
@@ -0,0 +1,15 @@
+<script setup>
+  const props = defineProps({
+    sesson: Number
+  });
+</script>
+
+<template>
+  <div class="flex items-center gap-2 max-md:hidden rtl:justify-end ltr:justify-start">
+    <svg class="w-7 h-7 text-light" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+      <circle cx="12" cy="12" r="11" stroke="currentColor" stroke-width="2" fill="none" />
+      <polygon points="10,8 16,12 10,16" fill="currentColor" />
+    </svg>
+    <span class="text-light font-bold text-base">فصل 2 قسمت 5</span>
+  </div>
+</template>
